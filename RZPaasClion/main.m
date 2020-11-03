60c8a43bf9bedefb72c9e18b0f141dabc7c57553
yxibng <yxibng@gmail.com>
2020-11-03 20:33:48 +0800

    Initial Commit

---
+++ RZPaasClion/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RZPaasClion
+//
+//  Created by yxibng on 2020/11/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
