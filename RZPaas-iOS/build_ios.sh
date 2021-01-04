3f0952573b2baa5d1941b4bbb74ccfb26c3975f0
yxibng <yxibng@gmail.com>
2021-01-04 22:01:30 +0800

    feat: ios framework info.plist

--- RZPaas-iOS/build_ios.sh
+++ RZPaas-iOS/build_ios.sh
@@ -1,7 +1,11 @@
 #/bin/sh
 
+
+rm -rf build
+
 mkdir -p build
 cd build 
+
 
 cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake -DPLATFORM=OS64COMBINED
 cmake --build . --config Release
